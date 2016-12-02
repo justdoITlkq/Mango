084360f0de5df4f04504a03c7f97827c4923c448
yangxiaobinhaoshuai <1154916418@qq.com>
2016-12-02 16:07:32 +0800

    compile 了  rengwuxian 的 md edittext

--- src/main/java/com/handsomeyang/mango/base/BaseActivityMaster.java
+++ src/main/java/com/handsomeyang/mango/base/BaseActivityMaster.java
@@ -9,6 +9,7 @@ import java.util.List;
  */
 
 public class BaseActivityMaster {
+
   private static List<BaseActivity> mBaseActivities = new ArrayList<>();
   private static BaseActivityMaster sMBaseActivityMaster;
 
