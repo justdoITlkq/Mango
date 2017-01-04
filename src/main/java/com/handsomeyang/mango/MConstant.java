c0a55b413d0527b333ac507f55cac3a1fa8e6db4
yangxiaobinhaoshuai <1154916418@qq.com>
2017-01-04 16:55:01 +0800

    修复日志功能

--- src/main/java/com/handsomeyang/mango/MConstant.java
+++ src/main/java/com/handsomeyang/mango/MConstant.java
@@ -5,25 +5,6 @@ package com.handsomeyang.mango;
  */
 
 public class MConstant {
-  // 三方工具类Readme.md   https://github.com/Blankj/AndroidUtilCode/blob/master/README-CN.md
-  //smartTablayout
-  /**
-   * 联网retrofit +rxjava
-   * 组件通信 rxbus
-   * 异步处理rxjava
-   * 图片 frecso   hongyang 的一整套图片解决方案
-   * base activity fragment application
-   *
-   *
-   * rengwuxian 的 material design edittext
-   * ios 和 md  风格的dialog 和 progressbar  自己基于smoothprogressbar 封装了两个进度条
-   * hongyang的 log
-   *
-   * DI 依赖注入 dragger 2+butterKnife view
-   * 写医生端用 mvp+dragger 2+ rxjava +mvp
-   *
-   */
-
   /**
    * 正则表达式:验证密码(不包含特殊字符)
    * 大小写字母，数字，6-16位
--- src/main/java/com/handsomeyang/mango/output/L.java
+++ src/main/java/com/handsomeyang/mango/output/L.java
@@ -1,9 +1,13 @@
 package com.handsomeyang.mango.output;
 
+import com.elvishew.xlog.XLog;
+
 /**
  * Created by HandsomeYang on 2016/9/12.
  */
 
 public class L {
-
+  public static void e(Object object) {
+    XLog.e(object);
+  }
 }
